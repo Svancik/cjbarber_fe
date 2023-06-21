6ccc061172212dc7c363c4ef232cd6a043b36953
Svancik <thesvancik@gmail.com>
2023-06-21 21:26:03 +0200

    sliderInfo

--- src/components/sliderInfo/SliderInfo.jsx
+++ src/components/sliderInfo/SliderInfo.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import "./sliderInfo.css";
 
 export const SliderInfo = () => {
@@ -12,18 +14,56 @@ export const SliderInfo = () => {
       </div>
       <div className="bottom">
         <div className="row">
-          <div className="left"></div>
-          <div className="right">
-            <span>Orlova 135, Roudnice nad Labem</span>
-          </div>
+          <LocationOnIcon className="mui" />
+          <span>Orlova 135, Roudnice nad Labem</span>
         </div>
 
         <div className="row">
-          <div className="left"></div>
-          <div className="right">
-            <span>Telefon: 777 606 447</span>
-          </div>
+          <LocalPhoneIcon className="mui" />
+          <span>777 606 447</span>
         </div>
+
+        <div className="row">
+          <hr />
+          <table className="openingHours">
+            <tr>
+              <td className="day">Pondělí: </td>
+              <td className="hours">zavřeno</td>
+            </tr>
+
+            <tr>
+              <td className="day">Úterý: </td>
+              <td className="hours">10:00 - 22:00</td>
+            </tr>
+
+            <tr>
+              <td className="day">Středa: </td>
+              <td className="hours">10:00 - 22:00</td>
+            </tr>
+
+            <tr>
+              <td className="day">Čtvrtek: </td>
+              <td className="hours">10:00 - 22:00</td>
+            </tr>
+
+            <tr>
+              <td className="day">Pátek: </td>
+              <td className="hours">10:00 - 22:00</td>
+            </tr>
+
+            <tr>
+              <td className="day">Sobota: </td>
+              <td className="hours">10:00 - 22:00</td>
+            </tr>
+
+            <tr>
+              <td className="day">Neděle: </td>
+              <td className="hours">zavřeno</td>
+            </tr>
+          </table>
+        </div>
+
+        <button className="reservation">Online Rezervace</button>
       </div>
     </div>
   );
